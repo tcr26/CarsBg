bac1adf9db4bdab3ec5d53025c61c0107460f4bd
tcr26 <dcrow@abv.bg>
2017-03-06 20:28:48 +0200

    CarsBg Project

---
+++ CarsBgPages/CarsBgLoginForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+
+namespace CarsBgPages
+{
+    class CarsBgLoginForm
+    {
+        public string urlToCarsBgHomePage = "http://www.cars.bg";
+
+        [FindsBy(How = How.Id, Using = "loginlink")]
+        public IWebElement CarsBgLoginButtonElement { get; set; }
+
+        [FindsBy(How = How.Id, Using = "loginForm")]
+        public IWebElement CarsBgLoginFormElement { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "/html/body/table[2]/tbody/tr/td/table/tbody/tr/td[3]/table/tbody/tr/td[6]/div/form/table[1]/tbody/tr[4]/td[1]/input")]
+        public IWebElement CarsBgLoginFormPrivateSeller { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "html body table.site_top_head tbody tr td table tbody tr td table tbody tr td div#login_panel.login_panel form#loginForm table#phone_table tbody tr td input")]
+        public IWebElement LoginFormPrivateSellerMobilePhoneElement { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "html body table.site_top_head tbody tr td table tbody tr td table tbody tr td div#login_panel.login_panel form#loginForm table#login_table tbody tr td input")]
+        public IWebElement LoginFormPrivateSellerPassword { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "/html/body/table[2]/tbody/tr/td/table/tbody/tr/td[3]/table/tbody/tr/td[6]/div/form/table[1]/tbody/tr[4]/td[2]/input")]
+        public IWebElement CarsBgLoginFormCompany { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "html body table.site_top_head tbody tr td table tbody tr td table tbody tr td div#login_panel.login_panel form#loginForm table#username_table tbody tr td input")]
+        public IWebElement LoginFormCompanyUserName { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "html body table.site_top_head tbody tr td table tbody tr td table tbody tr td div#login_panel.login_panel form#loginForm table#login_table tbody tr td input")]
+        public IWebElement LoginFormCompanyPassword { get; set; }
+    }
+}
---
+++ CarsBgTests/CarsBgMainPageTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Firefox;
+using CarsBgPages_HomePage;
+
+namespace CarsBg_HomePage_Tests
+{
+    [TestClass]
+    public class CarsBgHomePageTests
+    {
+        public CarsBgHomePageTests()
+        {
+            CarsBgHomePageClass = new CarsBgHomePage();
+            Driver = new FirefoxDriver();
+            Driver.Manage().Window.Maximize();
+            Waiter = new WebDriverWait(Driver, TimeSpan.FromSeconds(20));
+            Driver.Navigate().GoToUrl(CarsBgHomePageClass.urlToCarsBgHomePage);
+        }
+
+        public CarsBgHomePage CarsBgHomePageClass { get; set; }
+
+        public IWebDriver Driver { get; set; }
+
+        public WebDriverWait Waiter { get; set; }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            PageFactory.InitElements(Driver, CarsBgHomePageClass);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            Driver.Dispose();
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void GoToCarsBgHomePage()
+        {
+            string expectedPageTitle = "cars.bg - Търсите Автомобили? Нови, buyback, лизингови и употребявани коли";
+            var actualPageTitle = Driver.Title;
+
+            Assert.AreEqual(expectedPageTitle, actualPageTitle);
+        }
+    }
+}
